eb8670857edfec39d93bca122dd1d5dffc06ac5d
jose <pintovillamar@outlook.com>
2022-09-21 19:00:04 -0500

    create dependecies WITH PASSWORDS! AND PORTS!

---
+++ project/connect.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import psycopg2
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(
+    host="127.0.0.1",
+    port="5432",
+    database="prueba",
+    user="postgres",
+    password="1234")
+
+cursor = conn.cursor()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres@127.0.0.1:5432/prueba"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
