c2285fed2943126caba72331a5e7827f0dda07bb
jose <pintovillamar@outlook.com>
2022-09-24 21:53:02 -0500

    Replaced Student_schema for STUDENTS_schema

--- project/Student.py
+++ project/Student.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from connect import conn # import conn de connect
 from connect import app
 from connect import db, ma # SQLAlchemy Marshmelow
+from User import User # from User import User ForeingKey use only
 
 cursor = conn.cursor()
 
@@ -10,47 +11,62 @@ class Student(db.Model):
     std_id = db.Column(db.Integer, primary_key=True)
     std_regular = db.Column(db.Boolean())
     std_year = db.Column(db.Date())
-    user_usr_id = db.Column(db.Integer, db.ForeignKey(User.usr_id))
+    usr_id = db.Column(db.Integer, db.ForeignKey(User.usr_id))
+
+    def __init__(self, usr_id, std_regular, std_year):
+        self.usr_id = usr_id
+        self.std_regular = std_regular
+        self.std_year = std_year
+        
+
+db.create_all()
 
 class StudentSchema(ma.Schema):
     class Meta:
         fields = (
             'std_id',
+            'usr_id',
             'std_regular',
-            'std_year',
-            'user_usr_id'
+            'std_year'
         )
- 
-db.create_all()
 
-Student_schema = StudentSchema()
-Student_schema = StudentSchema(many=True)
+student_schema = StudentSchema()
+students_schema = StudentSchema(many=True)
 
+
+#Create Teacher
 @app.route('/create_student', methods=['POST'])
 def create_student():
     print(request.json)
 
+    usr_id = request.json['user_usr_id']
     std_regular = request.json['std_regular']
     std_year = request.json['std_year']
 
-    new_student = Student(std_regular, std_year)
+    new_student = Student(usr_id, std_regular, std_year)
     db.session.add(new_student)
     db.session.commit()
 
-    return Student_schema.jsonify(new_student)
+    return student_schema.jsonify(new_student)
 
+
+# List all teachers
 @app.route('/students', methods=['POST'])
 def students():
     all_students = Student.query.all()
-    result = Student_schema.dump(all_students) # lo vuelve serializable
+    result = students_schema.dump(all_students) # lo vuelve serializable
     return jsonify(result)
 
+
+# Search for a teacher by id
 @app.route('/student/<std_id>', methods=['POST'])
 def student(std_id):
     result = Student.query.get(std_id)
-    result = Student_schema.dump(result) # lo vuelve serializable
+    result = student_schema.dump(result) # lo vuelve serializable
     return jsonify(result)
 
+
+# Update a teacher by id
 @app.route('/update_student/<id>', methods=['POST'])
 def update_student(std_id):
     student = Student.query.get(std_id)
@@ -61,27 +77,29 @@ def update_student(std_id):
     student.std_year = std_year
 
     db.session.commit()
-    return Student_schema.jsonify(student)
+    return student_schema.jsonify(student)
 
+
+# Delete a teacher by id
 @app.route('/delete_student/<id>', methods=['POST'])
 def delete_student(std_id):
     student = Student.query.get(std_id)
     db.session.delete(student)
     db.session.commit()
-    return Student_schema.jsonify(student)
+    return student_schema.jsonify(student)
 
-## SELECT * FROM STUDENT
+# ## SELECT * FROM STUDENT
 
-@app.route('/select_students', methods=['POST'])
-def select_students():
-    cursor.execute("select * from student")
-    #data = cursor.fetchone() # obtiene un registro
-    rv = cursor.fetchall()
+# @app.route('/select_students', methods=['POST'])
+# def select_students():
+#     cursor.execute("select * from student")
+#     #data = cursor.fetchone() # obtiene un registro
+#     rv = cursor.fetchall()
 
-    data = []
-    content = {}
-    for result in rv:
-        content = {'std_regular': result[0], 'std_id': result[1], 'std_year': result[2]}
-        data.append(content)
-        content = {}
-    return jsonify(data)
+#     data = []
+#     content = {}
+#     for result in rv:
+#         content = {'std_regular': result[0], 'std_id': result[1], 'std_year': result[2]}
+#         data.append(content)
+#         content = {}
+#     return jsonify(data)
