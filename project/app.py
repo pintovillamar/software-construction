29aa0d9ad9aebfcd974d9d65cc23f6b05944db8f
jose <pintovillamar@outlook.com>
2022-09-24 21:53:23 -0500

    added Student and Group to the import lines

--- project/app.py
+++ project/app.py
@@ -2,6 +2,8 @@ from Courses import *
 from User import *
 from UserType import *
 from Teacher import *
+from Student import *
+from Group import *
 
 if __name__ == "__main__":
     app.run(debug=True)
