33f144ac969ccf4a5a7c50ab5b6f602c71f229f4
André Cruces <ecrucesg@ulasalle.edu.pe>
2022-10-04 22:11:41 -0500

    Create Courses_blueprint.py

---
+++ project2/backend/blueprints/Courses_blueprint.py
@@ -0,0 +1,53 @@
+import json
+from flask import Flask
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from werkzeug.utils import secure_filename
+
+from flask_cors import CORS, cross_origin 
+
+from backend.models.Courses_model import Courses_Model
+model = Courses_Model()
+
+
+courses_blueprint = Blueprint('/courses_blueprint', __name__)
+
+# Create a course
+@courses_blueprint.route('/create_course', methods=['POST'])
+@cross_origin()
+def create_course():
+    print(request.json['cur_name'])
+    print(request.json['cur_des'])
+    content = model.create_course(request.json['cur_name'])    
+    content = model.create_course(request.json['cur_des']) 
+    return jsonify(content)
+
+# List course with ID
+@courses_blueprint.route('/course/<cur_id>', methods=['POST'])
+@cross_origin()
+def course(cur_id):
+    print(request.json['cur_name'])
+    print(request.json['cur_des'])
+    content = model.course(request.json[cur_id])
+    return jsonify(content)
+    #return jsonify(model.course(int(request.json['cur_id'])))
+
+# List all courses
+@courses_blueprint.route('/courses', methods=['POST'])
+@cross_origin()
+def courses():
+    return jsonify(model.courses())
+
+# Update course.route('/update_course/<cur_id>', methods=['POST'])
+@cross_origin()
+def update_course(cur_id):
+    # return jsonify(model.update_teacher(int(request.json['tea_id']), request.json['name']))
+    content = model.update_teacher(request.json['cur_name'])
+    content = model.update_teacher(request.json['cur_des'])
+    return jsonify(content)
+
+# @course_blueprint.route('/delete_course', methods=['POST'])
+# @cross_origin()
+# def delete_course():
+#     return jsonify(model.delete_course(int(request.json['id'])))
---
+++ project2/backend/blueprints/Teachers_blueprint.py
@@ -0,0 +1,58 @@
+import json
+from flask import Flask
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from werkzeug.utils import secure_filename
+
+from flask_cors import CORS, cross_origin 
+
+from backend.models.Teachers_model import Teachers_Model
+model = Teachers_Model()
+
+
+teachers_blueprint = Blueprint('/teachers_blueprint', __name__)
+
+# Create a teacher
+@teachers_blueprint.route('/create_teacher', methods=['POST'])
+@cross_origin()
+def create_teacher():
+    print(request.json['usr_id'])
+    print(request.json['tea_type'])
+    print(request.json['tea_cat'])
+    content = model.create_teacher(request.json['usr_id'])    
+    content = model.create_teacher(request.json['tea_type']) 
+    content = model.create_teacher(request.json['tea_cat']) 
+    return jsonify(content)
+
+# List teacher with ID
+@teachers_blueprint.route('/teacher/<tea_id>', methods=['POST'])
+@cross_origin()
+def teacher(tea_id):
+    print(request.json['usr_id'])
+    print(request.json['tea_type'])
+    print(request.json['tea_cat'])
+    content = model.teacher(request.json[tea_id])
+    return jsonify(content)
+    #return jsonify(model.teacher(int(request.json['tea_id'])))
+
+# List all teachers
+@teachers_blueprint.route('/teachers', methods=['POST'])
+@cross_origin()
+def teachers():
+    return jsonify(model.teachers())
+
+# Update teacher by ID
+@teachers_blueprint.route('/update_teacher/<tea_id>', methods=['POST'])
+@cross_origin()
+def update_teacher(tea_id):
+    # return jsonify(model.update_teacher(int(request.json['tea_id']), request.json['name']))
+    content = model.update_teacher(request.json['usr_id'])
+    content = model.update_teacher(request.json['tea_type'])
+    content = model.update_teacher(request.json['tea_cat'])
+    return jsonify(content)
+
+# @teacher_blueprint.route('/delete_teacher', methods=['POST'])
+# @cross_origin()
+# def delete_teacher():
+#     return jsonify(model.delete_teacher(int(request.json['id'])))
