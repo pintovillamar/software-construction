0c898c220605440b9a3070471e9d09bee4d10780
jose <pintovillamar@outlook.com>
2022-10-05 21:45:31 -0500

    just replaced the DELETED json to model

--- project2/backend/blueprints/User_type_blueprint.py
+++ project2/backend/blueprints/User_type_blueprint.py
@@ -1,15 +1,10 @@
-import json
-from flask import Flask
 from flask import Blueprint
 from flask import request
 from flask import jsonify
-from werkzeug.utils import secure_filename
-
 from flask_cors import CORS, cross_origin 
 
 from backend.models.User_type_model import User_Type_Model
 model = User_Type_Model()
-
 
 user_type_blueprint = Blueprint('/user_types_blueprint', __name__)
 
@@ -25,16 +20,13 @@ def create_user_type():
 @user_type_blueprint.route('/user_type/<ust_id>', methods=['POST'])
 @cross_origin()
 def user_type(ust_id):
-    print(request.json['ust_id'])
-    content = model.user_type(request.json['ust_id'])
+    content = model.user_type(ust_id)
     return jsonify(content)
-    #return jsonify(model.user_type(int(request.json['ust_id'])))
 
 # List all user types
 @user_type_blueprint.route('/user_types', methods=['POST'])
 @cross_origin()
 def user_types():
-    # return jsonify(model.user_types())
     content = model.user_types()
     return jsonify(content)
 
@@ -42,10 +34,12 @@ def user_types():
 @user_type_blueprint.route('/update_user_type/<ust_id>', methods=['POST'])
 @cross_origin()
 def update_user_type(ust_id):
-    # return jsonify(model.update_user_type(int(request.json['ust_id']), request.json['name']))
-    return jsonify(model.update_user_type(request.json['ust_name']))
+    content = model.update_user_type(ust_id, request.json['ust_name'])
+    return jsonify(content)
 
-# @user_type_blueprint.route('/delete_user_type', methods=['POST'])
-# @cross_origin()
-# def delete_user_type():
-#     return jsonify(model.delete_user_type(int(request.json['id'])))
+# Delete user type by ID
+@user_type_blueprint.route('/delete_user_type/<ust_id>', methods=['POST'])
+@cross_origin()
+def delete_user_type(ust_id):
+    model.delete_user_type(ust_id)
+    return f"User type {ust_id} deleted succesfully" 
--- project2/backend/models/User_type_model.py
+++ project2/backend/models/User_type_model.py
@@ -2,6 +2,7 @@ from backend.models.connection_pool import getcursor
 from flask import jsonify
 from backend.models.conn import db
 from backend.models.conn import ma
+from werkzeug.utils import secure_filename
 
 class User_type(db.Model):
     ust_id = db.Column(db.Integer, primary_key=True)
@@ -27,40 +28,37 @@ class User_Type_Model:
         new_user_type = User_type(ust_name)
         db.session.add(new_user_type)
         db.session.commit()
-        return userType_schema.dump(new_user_type)
+        result = userType_schema.dump(new_user_type)
+        return result
 
     # List user type with ID
     def user_type(self, ust_id):
-        # user_type = User_types.query.get(ust_id)
-        # return userType_schema.dump(user_type)
         user_type = User_type.query.get(ust_id)
         result = userType_schema.dump(user_type)
-        # db.session.commit()
-        print(result)
-        return jsonify(result)
+        return result
 
     # List all user types
     def user_types(self):
-        # all_user_types = User_types.query.all()
-        # result = userTypes_schema.dump(all_user_types)
-        # db.session.commit()
-        # return result
         all_users_types = User_type.query.all()
         result = userTypes_schema.dump(all_users_types)
-        print(result)
         return result
 
     # Update user type by ID
-    def update_user_type(self, ust_id):
+    def update_user_type(self, ust_id, ust_name):
         user_type = User_type.query.get(ust_id)
-        user_type.ust_name = User_type.ust_name
+        user_type.ust_name = ust_name
         db.session.commit()
-        return userType_schema.jsonify(user_type)
+        result = userType_schema.dump(user_type)
+        return result
 
     # Delete user type by ID
     def delete_user_type(self, ust_id):
         user_type = User_type.query.get(ust_id)
+        # user_type.ust_name = ust_name
         db.session.delete(user_type)
         db.session.commit()
         return userType_schema.jsonify(user_type)
+
+        # to do 
+        # 1. return json with ust_id AND ust_name
 
