8e4862227fe502b038d2485f61e31763c0645a50
yoshiro1786 <66931868+ymirandav@users.noreply.github.com>
2022-10-06 11:37:44 -0500

    Edit del Student

--- project2/backend/blueprints/Students_blueprint.py
+++ project2/backend/blueprints/Students_blueprint.py
@@ -4,54 +4,52 @@ from flask import Blueprint
 from flask import request
 from flask import jsonify
 from werkzeug.utils import secure_filename
-
 from flask_cors import CORS, cross_origin 
 
-from backend.models.Students_Model import Students_Model
-model = Students_Model()
+from backend.models.Student_model import Student_Model
+model = Student_Model()
 
-students_blueprint = Blueprint('/students_blueprint', __name__)
+student_blueprint = Blueprint('/students_blueprint', __name__)
 
 # Create a student
-@students_blueprint.route('/create_students', methods=['POST'])
+@student_blueprint.route('/create_student', methods=['POST'])
 @cross_origin()
-def create_students():
+def create_student():
     print(request.json['usr_id'])
     print(request.json['std_regular'])
     print(request.json['std_year'])
-    content = model.create_students(request.json['usr_id'])
-    content = model.create_students(request.json['std_regular'])    
-    content = model.create_students(request.json['std_year'])    
+    content = model.create_student(request.json['usr_id'])
+    content = model.create_student(request.json['std_regular'])    
+    content = model.create_student(request.json['std_year'])    
     return jsonify(content)
 
 # List student with ID
-@students_blueprint.route('/student/<std_id>', methods=['POST'])
+@student_blueprint.route('/student/<std_id>', methods=['POST'])
 @cross_origin()
 def student(std_id):
-    print(request.json['usr_id'])
-    print(request.json['std_regular'])
-    print(request.json['std_year'])
-    content = model.student(request.json[std_id])
+    content = model.student(std_id)
     return jsonify(content)
-    #return jsonify(model.student(int(request.json['std_id'])))
 
 # List all students
-@students_blueprint.route('/students', methods=['POST'])
+@student_blueprint.route('/students', methods=['POST'])
 @cross_origin()
 def students():
-    return jsonify(model.students())
-
-# Update student by ID
-@students_blueprint.route('/update_student/<std_id>', methods=['POST'])
-@cross_origin()
-def update_student(std_id):
-    # return jsonify(model.update_student(int(request.json['std_id']), request.json['name']))
-    content = model.update_student(request.json['usr_id'])
-    content = model.update_student(request.json['std_regular'])
-    content = model.update_student(request.json['std_year'])
+    content = model.student()
     return jsonify(content)
 
-# @student_blueprint.route('/delete_student', methods=['POST'])
-# @cross_origin()
-# def delete_student():
-#     return jsonify(model.delete_student(int(request.json['id'])))
+# Update student by ID
+@student_blueprint.route('/update_student/<std_id>', methods=['POST'])
+@cross_origin()
+def update_student(std_id):
+    
+    content = model.update_student(std_id, 
+                                    request.json['usr_id'],
+                                    request.json['std_regular'],
+                                    request.json['std_year'])
+    return jsonify(content)
+
+@student_blueprint.route('/delete_student/<std_id>', methods=['POST'])
+@cross_origin()
+def delete_student(std_id):
+    model.delete_student(std_id)
+    return f"Student {std_id} deleted succsesfully"
