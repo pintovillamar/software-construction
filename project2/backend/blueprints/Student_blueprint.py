9e257b9d7f5c4ce99a2b94d2bbec8c141b913b71
jose <pintovillamar@outlook.com>
2022-10-12 14:17:49 -0500

    assets para latex

--- project2/backend/blueprints/Student_blueprint.py
+++ project2/backend/blueprints/Student_blueprint.py
@@ -9,7 +9,7 @@ from flask_cors import CORS, cross_origin
 from backend.models.Student_model import Student_Model
 model = Student_Model()
 
-student_blueprint = Blueprint('/students_blueprint', __name__)
+student_blueprint = Blueprint('/student_blueprint', __name__)
 
 # Create a student
 @student_blueprint.route('/create_student', methods=['POST'])
@@ -18,9 +18,11 @@ def create_student():
     print(request.json['usr_id'])
     print(request.json['std_regular'])
     print(request.json['std_year'])
-    content = model.create_student(request.json['usr_id'])
-    content = model.create_student(request.json['std_regular'])    
-    content = model.create_student(request.json['std_year'])    
+    print(request.json['std_vector'])
+    content = model.create_student(request.json['usr_id'],
+                                   request.json['std_regular'],
+                                   request.json['std_year'],
+                                   request.json['std_vector'])
     return jsonify(content)
 
 # List student with ID
@@ -45,7 +47,8 @@ def update_student(std_id):
     content = model.update_student(std_id, 
                                     request.json['usr_id'],
                                     request.json['std_regular'],
-                                    request.json['std_year'])
+                                    request.json['std_year'],
+                                    request.json['std_vector'])
     return jsonify(content)
 
 # Delete student by ID
@@ -54,3 +57,21 @@ def update_student(std_id):
 def delete_student(std_id):
     model.delete_student(std_id)
     return f"Student {std_id} deleted succsesfully"
+
+# test func
+@student_blueprint.route('/face_student/<std_id>', methods=['POST'])
+@cross_origin()
+def face_student(std_id):
+    content = model.face_student(std_id)
+    path_photo = model.face_student(std_id)
+    f = request.files['file']
+    filename = f.filename
+    path_v = content
+    print(path_photo)
+    return path_v
+
+# @student_blueprint.route('/convert_image_vector/<std_id>', methods=['POST'])
+# @cross_origin()
+# def convert_image_vector(std_id):
+#     content = model.convert_image_vector(std_id)
+#     return jsonify(content)
