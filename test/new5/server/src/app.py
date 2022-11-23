08e7a628ca03c99b1f3094f55f416b0961b3aee7
jose <pintovillamar@outlook.com>
2022-11-22 20:52:09 -0500

    updated app with courses @pintovillamar

--- test/new5/server/src/app.py
+++ test/new5/server/src/app.py
@@ -3,9 +3,11 @@ from database import app
 
 from blueprints.UserType_blueprint import user_type_blueprint
 from blueprints.User_blueprint import user_blueprint
+from blueprints.Course_blueprint import course_blueprint
 
 app.register_blueprint(user_type_blueprint)
 app.register_blueprint(user_blueprint)
+app.register_blueprint(course_blueprint)
 
 cors = CORS(app, supports_credentials=True)
 
